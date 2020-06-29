aee1fbf73a076015cfc8c440012163ff59f371ba
Daniel Walz <developer@foleysfinest.com>
2020-06-29 23:30:23 +0100

    Added script to build ffmpeg on OSX

---
+++ scripts/build_ffmpeg_osx.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pushd ../libraries
+mkdir build
+pushd build
+
+../ffmpeg/configure --enable-shared --enable-gpl --enable-libx264 --install-name-dir='@executable_path' --prefix=`pwd` --libdir=`pwd`/ffmpeg_libs
+make -j 8
+make install
+
+popd
+popd
